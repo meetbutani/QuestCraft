56089a1286af0351643919b6f06b2438187e75cb
namraravani <namraravani8@gmail.com>
2024-03-05 23:04:45 +0530

    Added_the_login_page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,13 +5,14 @@ import IntroPage from "./components/IntroPage";
 
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import RegisterPage from "./components/RegisterPage";
+import LoginPage from "./components/LoginPage";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<IntroPage />} />
-        <Route path="/login" element={<IntroPage />} />
+        <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
       </Routes>
     </Router>
---
+++ frontend/src/components/LoginPage.jsx
@@ -0,0 +1,102 @@
+import  { useState } from "react";
+import "react";
+import { Link } from "react-router-dom";
+import "tailwindcss/tailwind.css";
+import PasswordShowHideBtn from "./PasswordShowHideBtn";
+
+const LoginPage = () => {
+  
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const handlePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
+  return (
+    <div className="min-h-screen bg-background flex flex-col items-center justify-center">
+      <div className="w-full max-w-md">
+        <div className="bg-white shadow-md rounded-lg p-8">
+          <h1 className="text-5xl font-bold mb-6 text-center text-txtcol">
+            Login
+          </h1>
+          <div className="bg-txtcol w-auto h-0.5 mb-5" />
+          <form onSubmit={handleSubmit} className="w-full max-w-md">
+            
+            
+            <div className="mb-4">
+              <label
+                htmlFor="email"
+                className="block text-txtcol-900 text-sm font-bold mb-2"
+              >
+                Email
+              </label>
+              <input
+                type="email"
+                id="email"
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-txtcol leading-tight focus:outline-background"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div className="mb-4 relative">
+              <label
+                htmlFor="password"
+                className="block text-txtcol-900 text-sm font-bold mb-2"
+              >
+                Password
+              </label>
+              <input
+                type={showPassword ? "text" : "password"}
+                id="password"
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-txtcol leading-tight focus:outline-background"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <div className="absolute right-1 bottom-1 mt-2 mr-2 text-btntxtcol">
+                <PasswordShowHideBtn
+                  width={"30px"}
+                  fill={"#1C3342"}
+                  id={"pass1"}
+                  onClickPassShowHide={() => handlePasswordVisibility()}
+                />
+              </div>
+            </div>
+            <div className="flex items-center justify-center">
+              <button
+                type="submit"
+                className="bg-btnbg text-btntxtcol px-4 py-2 rounded-lg"
+              >
+                Login
+              </button>
+            </div>
+            <div className="flex justify-center mt-3 mb-3">
+              <span className="text-txtcol">
+                New To Our Website ?&nbsp;
+              </span>
+              <Link to="/register" className="text-background mr-2">
+                Regester Here
+              </Link>
+            </div>
+            <div className="bg-txtcol w-auto h-0.5 mb-3 mt-3" />
+            <div className="flex items-center justify-center">
+              <button
+                type="button"
+                className="bg-btnbg text-btntxtcol px-4 py-2 rounded-lg"
+              >
+                Sign in with Google
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
